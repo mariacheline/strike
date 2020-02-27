8ddefd3302825548409069e933c658ac9b6aa8d0
mariacheline <mariacheline@gmail.com>
2020-02-27 14:24:41 +0100

    Updates

---
+++ precache-manifest.5c381ef996a155331d32efa9c2941f7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722fe5f8705e067819220381783c59d9",
+    "url": "/strike/index.html"
+  },
+  {
+    "revision": "1d80e62fe51fc4b13c18",
+    "url": "/strike/static/js/2.241236c5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/strike/static/js/2.241236c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50cb4f15296d71e45a73",
+    "url": "/strike/static/js/main.bc7ea984.chunk.js"
+  },
+  {
+    "revision": "913d7695920681829dcb",
+    "url": "/strike/static/js/runtime-main.2ab3b945.js"
+  },
+  {
+    "revision": "19314b5f02cfc1e935bb5f39561f626a",
+    "url": "/strike/static/media/close.19314b5f.svg"
+  }
+]);
